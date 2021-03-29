4613490996738f081ee30556a5a07ab24de04b73
nirkaufman <nirkaufman@gmail.com>
2021-03-29 10:05:25 +0300

    angular modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+   <app-todolist></app-todolist>
+  `,
 })
 export class AppComponent {}
